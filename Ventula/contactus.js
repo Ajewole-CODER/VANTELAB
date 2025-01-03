59df0485cf485e0a1df04944c1ce106e277e45d0
SirBlessing <olaniyiolawale013@gmail.com>
2025-01-03 18:25:36 +0100

    Add files via upload

---
+++ Ventula/contactus.js
@@ -0,0 +1,12 @@
+let addedComment=[1,2];
+let commentTotal=addedComment.length
+document.getElementById("ja").innerHTML=commentTotal;
+
+
+let addedLikes=[1,2,3];
+let LikesTotal=addedLikes.length;
+document.getElementById("likes").innerHTML=LikesTotal;
+
+let addedcart=[1,2,3,"blessing"];
+let cartTotal=addedcart.length;
+document.getElementById("cart").innerHTML=cartTotal;
